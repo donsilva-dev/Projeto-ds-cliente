f6a9e0c236971d770e85600cb2b0eab277f6a0df
Douglas <douglas.n.silva30@gmail.com>
2023-01-27 00:17:02 -0300

    Pagination

--- backend/src/main/java/com/apiclient/clientapi/services/ClientService.java
+++ backend/src/main/java/com/apiclient/clientapi/services/ClientService.java
@@ -1,13 +1,13 @@
 package com.apiclient.clientapi.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,15 +21,21 @@ public class ClientService {
 
 	@Autowired
 	private ClientRepository repository;
-
+/*
 	@Transactional(readOnly = true)
 	public List<ClientDTO> findAll() {
 		List<Client> list = repository.findAll();
 
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
-
 	}
-
+*/
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+	}
+	
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
@@ -79,4 +85,5 @@ public class ClientService {
 			throw new ResourceNotFoundException("Não tente novamente sem corrigir o problema. id provavelmente deletado.");
 		}
 	}
+
 }
